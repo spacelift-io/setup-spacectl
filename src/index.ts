1f9babcb50f853ac19c71fb9fe3b4c8294d51245
Peter Deme <snypox@gmail.com>
2022-12-20 20:23:23 +0100

    Bump `npm` packages (#5)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import { installAndGetFolder } from "./install";
 
 async function run(): Promise<void> {
-  let binaryFolder = await installAndGetFolder();
+  const binaryFolder = await installAndGetFolder();
   core.addPath(binaryFolder);
 
   core.info("Added spacectl to PATH: " + binaryFolder);
